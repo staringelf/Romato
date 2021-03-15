b6f805dca93de2241a72cec99ed6d8f984e31a74
Manish Sajwan <msoffice37@gmail.com>
2021-03-15 05:36:52 -0700

    fix http handler

--- handlers/httpHandler.js
+++ handlers/httpHandler.js
@@ -1,4 +1,4 @@
-exports.redirectToHttps(req, res, next) {
+exports.redirectToHttps = (req, res, next) => {
   if(req.headers['x-forwarded-proto'] === 'http') {
     return res.redirect('https://' + req.headers.host + req.url);
   }
